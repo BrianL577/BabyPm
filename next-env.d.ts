6a2e035ee8241cc719309dc6ad2258843b64f0fa
BrianL577 <67929160+BrianL577@users.noreply.github.com>
2026-06-21 12:06:59 -0700

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
 
-export default nextConfig;
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
